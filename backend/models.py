854f2925be01d9a9a8300a3bb2613b221842f8d8
Walter Okas <walter@dmarkmobile.com>
2022-09-09 13:19:08 +0300

    Activated and tested frontend questions and categories endpoints

--- backend/models.py
+++ backend/models.py
@@ -4,7 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 import json
 
 database_name = 'trivia'
-database_path = 'postgresql://{}/{}'.format('localhost:5432', database_name)
+database_path = 'postgresql://{}/{}'.format('walter:password@localhost:5432', database_name)
+#SQLALCHEMY_DATABASE_URI = 'postgresql://walter:password@localhost:5432/fyyur_db'
 
 db = SQLAlchemy()
 
--- frontend/src/components/QuestionView.js
+++ frontend/src/components/QuestionView.js
@@ -22,9 +22,10 @@ class QuestionView extends Component {
 
   getQuestions = () => {
     $.ajax({
-      url: `/questions?page=${this.state.page}`, //TODO: update request URL
+      url: `http://localhost:5000/questions?page=${this.state.page}`, //TODO: update request URL
       type: 'GET',
       success: (result) => {
+        //console.log(result)
         this.setState({
           questions: result.questions,
           totalQuestions: result.total_questions,
