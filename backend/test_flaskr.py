814ef165d49f0a6d04fa127f3d93eab3557f261c
Walter Okas <walter@dmarkmobile.com>
2022-09-16 13:29:16 +0300

    Added other TDD test scenarios in test_flaskr file

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -2,9 +2,22 @@ import os
 import unittest
 import json
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+load_dotenv()
 
 from flaskr import create_app
 from models import setup_db, Question, Category
+
+
+#SETUP DATABASE CONNECTION CONFIG
+#=========================================
+database_name=os.getenv('DATABASE_NAME')
+engine=os.getenv('ENGINE')
+username=os.getenv('USERNAME')
+password=os.getenv('PASSWORD')
+domain=os.getenv('DOMAIN')
+port=os.getenv('PORT')
+database_path = f'{engine}://{username}:{password}@{domain}:{port}/{database_name}'
 
 
 class TriviaTestCase(unittest.TestCase):
@@ -14,8 +27,8 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_name = database_name
+        self.database_path = f'{engine}://{username}:{password}@{domain}:{port}/{database_name}'
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -34,11 +47,41 @@ class TriviaTestCase(unittest.TestCase):
     Write at least one test for each test for successful operation and for expected errors.
     """
 
-    def test_get_questions(self):
-        res = self.client().get('/')
+    def test_listCategories(self):
+        res = self.client().get('/categories')
 
         self.assertEqual(res.status_code, 200)
 
+    def test_get_questions(self):
+        res = self.client().get('/questions')
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_delete_question(self):
+        res = self.client().get('/questions/<int:id>')
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_add_question(self):
+        res = self.client().get('/questions/add')
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_search_questions(self):
+        res = self.client().get('/questions/search')
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_get_questions_based_on_category(self):
+        res = self.client().get('/category/<int:id>/questions')
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_play_quiz(self):
+        res = self.client().get('/quizzes')
+
+        self.assertEqual(res.status_code, 200)
+        # self.assert(len(res) >= 1)
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
